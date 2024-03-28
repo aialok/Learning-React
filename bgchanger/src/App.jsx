59e42acd2dc454e7a9fe1a4daab23d56790a364c
aialok <alokgupta1560@gmail.com>
2024-03-28 12:20:32 +0530

    Wrapper components

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,127 +1,39 @@
-import React, { useState } from "react";
-import ToDo from "./components/ToDo";
+import React from "react";
 
-export default function App() {
-  const [todos, setTodos] = useState([
-    {
-      title: "Learn React",
-      description: "Learn how to use React",
-      completed: false,
-    },
-    {
-      title: "Learn Vite",
-      description: "Learn how to use Vite",
-      completed: false,
-    },
-    {
-      title: "Learn Tailwind",
-      description: "Learn how to use Tailwind",
-      completed: false,
-    },
-  ]);
-
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [completed, setCompleted] = useState('off');
-
-  const onChange = (e) => {
-    const name = e.target.name;
-    let value = e.target.value;
-
-    if (name === "title") {
-      setTitle(value);
-    } else if (name === "description") {
-      setDescription(value);
-    } else if (name === "completed") {
-      console.log(value);
-      if(value === "on")
-      {
-        value = "off";
-      }
-      else{
-        value = "on";
-      }
-
-      e.target.value = value;
-      setCompleted(value);
-    }
-  };
-
-  const AddToDo = () => {
-    const newTodo = {
-      title: title,
-      description: description,
-      completed: completed,
-    };
-
-    setTodos([...todos, newTodo]);
-  };
-
+function App() {
   return (
     <div>
-       <div>
-        <input
-          type="text"
-          name="title"
-          id=""
-          className="border-2"
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          name="description"
-          id=""
-          className="border-2"
-          onChange={onChange}
-        />
-        <input
-          type="radio"
-          name="completed"
-          id=""
-          className="border-2"
-          onChange={onChange}
-          onClick={onChange}
-        />
-        <button className="bg-green-300 border-2" onClick={AddToDo}>
-          Add
-        </button>
-      </div>
-      <h1 className="text-3xl font-bold underline text-center">To Do List</h1>
-      {todos.map((todo, index) => (
-        <ToDo
-          key={index}
-          title={todo.title}
-          description={todo.description}
-          completed={todo.completed}
-        />
-      ))}
-     
+      {/* <WrapperComponent >
+        <h1>hello world</h1>
+        <p>How are u doing?</p>
+      </WrapperComponent> */}
+      <WrapperComponent>
+        <h1>hello world</h1>
+        <WrapperComponent>
+          <h1>hello world</h1>
+          <p>How are u doing?</p>
+        </WrapperComponent>
+        <p>How are u doing?</p>
+      </WrapperComponent>
     </div>
   );
 }
 
-// function AddToDo() {
-//   const [todo, setTodo] = useState("");
-
-//   const onChange = (e) => {
-//     setTodo(e.target.value);
-//   };
-
-//   const AddToDo = () => {
-//     console.log(todo);
-//   };
+// const TextComponent = () => {
 //   return (
 //     <div>
-//       <input
-//         type="text"
-//         name=""
-//         id=""
-//         className="border-2"
-//         onChange={onChange}
-//       />
-//       <button className="bg-green-300 border-2" onClick={AddToDo}>
-//         Add
-//       </button>
+//       <h1>hello world</h1>
 //     </div>
 //   );
-// }
+// };
+
+const WrapperComponent = ({ children }) => {
+  console.log(children);
+  return (
+    <div key={children.index} className="border-2 border-blue-500">
+      {children}
+    </div>
+  );
+};
+
+export default App;
